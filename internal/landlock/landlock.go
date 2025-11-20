4beeade1e295ee2fcffac482bb3be9f41fd86242
Paulo Gomes <paulo.gomes@suse.com>
2025-11-20 14:53:25 +0000

    landlock: Add docker credential helper detection Improve the user experience by exiting the application and informing users that docker credential helpers aren't supported w/ landlock and the workaround. It also provides an opt-in landlock enforcement for docker credential helper which is not comprehensive, and could change depending on distro-specific configuration.
    Signed-off-by: Paulo Gomes <paulo.gomes@suse.com>

--- internal/landlock/landlock.go
+++ internal/landlock/landlock.go
@@ -1,13 +1,16 @@
 package landlock
 
 import (
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
 	"github.com/landlock-lsm/go-landlock/landlock"
+	"github.com/landlock-lsm/go-landlock/landlock/syscall"
 )
 
 const dirMode = 0o700
@@ -50,25 +53,78 @@ func EnforceOrDie() {
 		rwDirs = append(rwDirs, cwd) // Needs write access to CWD for "product verify" subcommand
 	}
 
-	// We can't really restrict the network access at present
-	err = cfg.RestrictPaths(
+	rules := []landlock.Rule{
+		landlock.ROFiles(
+			"/etc/resolv.conf", // DNS resolution.
+		).IgnoreIfMissing(),
 		landlock.RWDirs(rwDirs...),
 		landlock.RODirs(
 			"/proc/self",
 			"/etc/ssl",                     // Root CA bundles to establish TLS.
 			"/var/lib/ca-certificates",     // Root CA bundles to establish TLS.
 			filepath.Join(home, ".docker"), // Docker config to access OCI/registries.
-		),
-		landlock.ROFiles(
-			"/etc/resolv.conf", // DNS resolution.
-		),
-	)
+		).IgnoreIfMissing(),
+	}
+
+	if helper, ok := credentialHelper(home); ok {
+		if val, ok := os.LookupEnv("LANDLOCK_CREDENTIAL_HELPER"); ok && strings.EqualFold(val, "true") {
+			rules = append(rules, helper...)
+		} else {
+			fmt.Println("ERR: cannot use landlock with docker-credential helpers: disable landlock with LANDLOCK_MODE=off")
+			os.Exit(1)
+		}
+	}
+
+	err = cfg.RestrictPaths(rules...)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to enforce landlock policies (requires Linux 5.13+): %v\n", err)
 		if val == "on" {
 			os.Exit(2)
 		}
 	}
+}
+
+func credentialHelper(home string) ([]landlock.Rule, bool) {
+	path := filepath.Join(home, ".docker/config.json")
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, false
+	}
+
+	defer f.Close()
+
+	var cfg dockerConfig
+	d := json.NewDecoder(f)
+	err = d.Decode(&cfg)
+	if err != nil {
+		return nil, false
+	}
+
+	if cfg.CredsStore == "" {
+		return nil, false
+	}
+
+	fullPath, err := exec.LookPath("docker-credential-" + cfg.CredsStore)
+	if err == nil {
+		rules := []landlock.Rule{
+			execFile(fullPath, "/bin/dbus-launch"),
+			landlock.RODirs("/lib", "/lib64", "/usr/lib", "/usr/lib64", "/proc/self"),
+			landlock.ROFiles("/etc/ld.so.cache", "/var/lib/dbus/machine-id", "/etc/machine-id").IgnoreIfMissing(),
+			landlock.ROFiles("/dev/null", "/dev/zero"),
+		}
+
+		return rules, true
+	}
+
+	return nil, false
+}
+
+func execFile(path ...string) landlock.FSRule {
+	return landlock.PathAccess(syscall.AccessFSExecute|syscall.AccessFSReadFile, path...)
+}
+
+type dockerConfig struct {
+	CredsStore string `json:"credsStore"`
 }
 
 func ensureDirs(dirs []string) {
