d93bbca1d9c47129c67b2300b681a0ca183241c4
Guilherme Macedo <guilherme@gmacedo.com>
2026-04-17 09:06:59 -0300

    Pipeline security hardening
    Signed-off-by: Guilherme Macedo <guilherme@gmacedo.com>

--- hack/base.mk
+++ hack/base.mk
@@ -1,4 +1,4 @@
-GOLANGCI_VERSION ?= v2.11.4
+GOLANGCI_VERSION ?= 8f3b0c7ed018e57905fbd873c697e0b1ede605a5 # v2.11.4
 TOOLS_BIN := $(shell mkdir -p build/tools && realpath build/tools)
 
 VERSION = $(shell git tag -l --contains HEAD | head -n 1)
@@ -17,8 +17,7 @@ LDFLAGS = -ldflags "-extldflags -s -w -X github.com/rancherlabs/slsactl/cmd.vers
 GOLANGCI = $(TOOLS_BIN)/golangci-lint-$(GOLANGCI_VERSION)
 $(GOLANGCI):
 	rm -f $(TOOLS_BIN)/golangci-lint*
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/$(GOLANGCI_VERSION)/install.sh | sh -s -- -b $(TOOLS_BIN) $(GOLANGCI_VERSION)
-	mv $(TOOLS_BIN)/golangci-lint $(TOOLS_BIN)/golangci-lint-$(GOLANGCI_VERSION)
+	$(call go-install-tool,$(GOLANGCI),github.com/golangci/golangci-lint/v2/cmd/golangci-lint@$(GOLANGCI_VERSION))
 
 # go-install-tool will 'go install' any package $2 and install it as $1.
 define go-install-tool
